6f92fe287c684f8d73d1ade87d828ceccd2ac22b
DinelyWellehewage <dinelywh78@gmail.com>
2024-09-14 10:49:40 +0200

    preparring additional data using jpql

--- src/main/java/io/javapro/ipl_dashboard/data/DataSourceConfig.java
+++ src/main/java/io/javapro/ipl_dashboard/data/DataSourceConfig.java
@@ -4,11 +4,14 @@ import javax.sql.DataSource;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 
 @Configuration
+
 public class DataSourceConfig {
      @Bean
+     @Primary
     public DataSourceTransactionManager customTransactionManager(DataSource dataSource) {
         return new DataSourceTransactionManager(dataSource);
     }
--- src/main/java/io/javapro/ipl_dashboard/data/JobCompletionNotificationListener.java
+++ src/main/java/io/javapro/ipl_dashboard/data/JobCompletionNotificationListener.java
@@ -24,49 +24,50 @@ public class JobCompletionNotificationListener implements JobExecutionListener {
 
     private static final Logger log = LoggerFactory.getLogger(JobCompletionNotificationListener.class);
 
-    private final JdbcTemplate jdbcTemplate;
+    //private final JdbcTemplate jdbcTemplate;
+    private final EntityManager em;
 
     @Autowired
-    public JobCompletionNotificationListener(JdbcTemplate jdbcTemplate) {
-        this.jdbcTemplate = jdbcTemplate;
+    public JobCompletionNotificationListener(EntityManager em) {
+        this.em = em;
     }
 
 
-    // @Override
-    // @Transactional
-    // public void afterJob(JobExecution jobExecution) {
-    //     if (jobExecution.getStatus() == BatchStatus.COMPLETED) {
-    //         log.info("!!! JOB FINISHED! Time to verify the results");
-    //         Map<String, Team> teamData = new HashMap<>();
+     @Override
+     @Transactional
+     public void afterJob(JobExecution jobExecution) {
+         if (jobExecution.getStatus() == BatchStatus.COMPLETED) {
+             log.info("!!! JOB FINISHED! Time to verify the results");
+             Map<String, Team> teamData = new HashMap<>();
 
-    //         em.createQuery("select m.team1, count(*) from Match m group by m.team1", Object[].class)
-    //                 .getResultList()
-    //                 .stream()
-    //                 .map(e -> new Team((String) e[0], (long) e[1]))
-    //                 .forEach(team -> teamData.put(team.getTeamName(), team));
+             em.createQuery("select m.team1, count(*) from Match m group by m.team1", Object[].class)
+                     .getResultList()
+                     .stream()
+                     .map(e -> new Team((String) e[0], (long) e[1]))
+                     .forEach(team -> teamData.put(team.getTeamName(), team));
 
-    //         em.createQuery("select m.team2, count(*) from Match m group by m.team2", Object[].class)
-    //                 .getResultList()
-    //                 .stream()
-    //                 .forEach(e -> {
-    //                     Team team = teamData.get((String) e[0]);
-    //                     team.setTotalMatches(team.getTotalMatches() + (long) e[1]);
-    //                 });
+             em.createQuery("select m.team2, count(*) from Match m group by m.team2", Object[].class)
+                     .getResultList()
+                     .stream()
+                     .forEach(e -> {
+                         Team team = teamData.get((String) e[0]);
+                         team.setTotalMatches(team.getTotalMatches() + (long) e[1]);
+                     });
 
-    //         em.createQuery("select m.matchWinner, count(*) from Match m group by m.matchWinner", Object[].class)
-    //                 .getResultList()
-    //                 .stream()
-    //                 .forEach(e -> {
-    //                     Team team = teamData.get((String) e[0]);
-    //                     if (team != null)
-    //                         team.setTotalWins((long) e[1]);
-    //                 });
+             em.createQuery("select m.matchWinner, count(*) from Match m group by m.matchWinner", Object[].class)
+                     .getResultList()
+                     .stream()
+                     .forEach(e -> {
+                         Team team = teamData.get((String) e[0]);
+                         if (team != null)
+                             team.setTotalWins((long) e[1]);
+                     });
 
-    //         teamData.values().forEach(team -> em.persist(team));
-    //         teamData.values().forEach(team -> System.out.println(team));
+             teamData.values().forEach(team -> em.persist(team));
+             teamData.values().forEach(team -> System.out.println(team));
 
-    //     }
-    // }
+         }
+     }
 
     @Override
     public void beforeJob(JobExecution arg0) {
@@ -75,15 +76,15 @@ public class JobCompletionNotificationListener implements JobExecutionListener {
     }
 
 
-    @Override
-    public void afterJob(JobExecution jobExecution) {
-        if (jobExecution.getStatus()==BatchStatus.COMPLETED) {
-            log.info("job finished time to verify results");
-
-            jdbcTemplate.query("SELECT team1, team2, date FROM match", 
-            (rs,row)->"Team 1 "+ rs.getString(1)+ " Team 2 "+ rs.getString(2)).forEach(str->System.out.println(str));
-        }else{
-            log.info("job did not complete succesfully");
-        }
-    }
+//    @Override
+//    public void afterJob(JobExecution jobExecution) {
+//        if (jobExecution.getStatus()==BatchStatus.COMPLETED) {
+//            log.info("job finished time to verify results");
+//
+//            jdbcTemplate.query("SELECT team1, team2, date FROM match",
+//            (rs,row)->"Team 1 "+ rs.getString(1)+ " Team 2 "+ rs.getString(2)).forEach(str->System.out.println(str));
+//        }else{
+//            log.info("job did not complete succesfully");
+//        }
+//    }
 }
